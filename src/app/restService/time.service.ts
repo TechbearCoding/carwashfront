b974ecb07df94186daf82e8b65a3cdf9a4169793
male <martins.leitarts@techbear.org>
2020-08-16 22:29:28 +0300

    days and hours returned from server

--- src/app/restService/time.service.ts
+++ src/app/restService/time.service.ts
@@ -19,9 +19,6 @@ export class TimeService {
   resolveItems(): Observable<any> {
     console.log('Request is sent!');
 
-    // this.http is a HttpClient library provide by @angular/common
-    // we are calling .get() method over this.http object
-    // this .get() method takes URL to call API
     return this.http.get(this.URL);
   }
 }

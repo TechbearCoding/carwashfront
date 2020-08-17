d06e4883bc7611e303352eafcb09ce2fea83f659
male <martins.leitarts@techbear.org>
2020-08-18 00:48:37 +0300

    submit page done

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { SERVICES } from '../mock-services';
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
-  styleUrls: ['./services.component.css']
+  styleUrls: ['./services.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class ServicesComponent implements OnInit {
 

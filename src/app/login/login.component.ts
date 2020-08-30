82c8537a2b813f092a91a850290d04a18ccd013f
male <martins.leitarts@techbear.org>
2020-08-31 00:28:40 +0300

    fixed login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../restService/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  submitted = false;
+
+  private postService: AuthService
+
+  authServ : AuthService;
+
+  constructor(authService: AuthService, private formBuilder: FormBuilder) {
+    this.authServ = authService;
+  }
+
 
   ngOnInit(): void {
+
+    this.loginForm = this.formBuilder.group({
+      uname: ['', Validators.required],
+      passw: ['', Validators.required],
+
+    });
+  }
+
+  login(){
+    alert("done");
+    this.authServ.login(this.loginForm.controls['uname'].value, this.loginForm.controls['passw'].value);
+    
   }
 
 }
---
+++ src/app/restService/auth.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../user';
+import { map } from 'rxjs/internal/operators/map';
+import { stringify } from 'querystring';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  [x: string]: any;
+
+  private currentUserSubject: BehaviorSubject<User>;
+  public currentUser: Observable<User>;
+  private readonly URL = 'http://127.0.0.1:8080/auth';
+
+  constructor(private http: HttpClient) {
+      this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+      this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  public get currentUserValue(): User {
+      return this.currentUserSubject.value;
+  }
+
+  login(username: string, password: string){
+    alert("done2")
+
+    this.http.post(this.URL,JSON.stringify([username, password])).subscribe(
+      data  => { 
+        localStorage.setItem('currentUser', JSON.stringify(username));
+      },
+      error  => {console.log("Error", error);}
+    );
+  }
+
+  logout() {
+      // remove user from local storage to log user out
+      localStorage.removeItem('currentUser');
+      this.currentUserSubject.next(null);
+  }
+}
